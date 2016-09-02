e0ba769adb2a0084ade9c4971f776df3dbbf7341
Mini Model <minimodel@metamodel.me>
2016-09-02 12:16:51 +0200

    Updated translations from transifex.

---
+++ contao/languages/de/tl_metamodel_attribute.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2016-08-29T20:12:23+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['levensthein_attributes']['0']          = 'Attribute zum Indexieren';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['levensthein_attributes']['1']          = 'Bitte wählen Sie alle Attribute, die indiziert werden sollen.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['levensthein_distance']['0']            = 'Maximaler Levensthein-Abstad';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['levensthein_distance']['1']            = 'Bitte wählen Sie den maximalen Abstand für den Levensthein-Algorithmus.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['levensthein_distance_distance']['0']   = 'Erlaubter Abstand';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['levensthein_distance_wordlength']['0'] = 'Maximale Wortlänge';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['rebuild_levensthein']                  = 'Suchindex neu aufbauen';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['levensthein']           = 'Levensthein-Suche';
+
